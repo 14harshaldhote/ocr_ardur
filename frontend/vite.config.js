27454283a5d19688a8dd9a2dbc17da7e8adb22f0
Harshal Dhote <dhoteh020@gmail.com>
2024-06-19 14:25:49 +0530

    ocr-test1

---
+++ frontend/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/, // This will handle both .js and .jsx files
+  },
+})
